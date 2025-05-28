c102b55400f4c12f2f78b261bcb4ce9dac71f04a
ynzung <syj91105@sookmyung.ac.kr>
2025-05-28 16:29:32 +0900

    Design: 디자인 시스템 상수화

---
+++ src/lib/constants/theme.ts
@@ -0,0 +1,23 @@
+export const COLORS = {
+  PRIMARY: '#5573E2',
+  TEXT_BLACK: '#3B3B3B',
+  TEXT_LIGHT_GREY: '#9A9A9A',
+  LINE_GREY: '#D9D9D9',
+  WHITE: '#FFFFFF',
+};
+
+export const FONT_SIZES = {
+  SM: '0.875rem',
+  BASE: '1rem',
+  LG: '1.125rem',
+  XL: '1.375rem',
+};
+
+export const BGCOLORS = {
+  BG_LIGHT_BLUE: '#EEF2F9',
+  BG_LIGHT_GREY: '#E1E1E1CC',
+};
+
+export const SHADOW = {
+  INPUT_SHADOW: 'inset 0px 0px 10px 0px rgba(85, 116, 226, 0.25)',
+};
