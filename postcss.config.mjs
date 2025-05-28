6c600fd9ddd36f3895936b54f72b4942db7c5c52
ynzung <syj91105@sookmyung.ac.kr>
2025-05-28 15:26:59 +0900

    fix: 테일윈드 설정 및 PostCSS 설정 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,9 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,27 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx}', // ✅ 여기에 경로가 layout.tsx 포함되게
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+
   theme: {
     extend: {
       colors: {
-        border: 'hsl(var(--border))',
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
+        mainColor: '#5573E2',
+        textBlack: '#3B3B3B',
+        textLightGrey: '#9A9A9A',
+        lineGrey: '#D9D9D9',
+        white: '#FFFFFF',
+        bgLightBlue: '#EEF2F9',
+        bgLightGrey: '#E1E1E1CC',
+      },
+      fontSize: {
+        sm: '0.875rem', // 14px
+        base: '1rem', // 16px
+        lg: '1.125rem', // 18px
+        xl: '1.375rem', // 22px
+      },
+      boxShadow: {
+        inputShadow: 'inset 0px 0px 10px 0px rgba(85, 116, 226, 0.25)',
       },
     },
   },
