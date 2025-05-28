2d2365404770b0a68e17ea395b7f97e59b46b8c8
ynzung <syj91105@sookmyung.ac.kr>
2025-05-28 16:31:08 +0900

    Feat: Input 컴포넌트 구현

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,14 @@
+interface InputPropTypes extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+export default function Input({ className = '', ...props }: InputPropTypes) {
+  return (
+    <div>
+      <input
+        {...props}
+        className={`text-textBlack font-base px-5 py-4 outline-none shadow-inputShadow rounded-[1.25rem] border-[1px] border-lineGrey focus:border-[1px] focus:border-mainColor placeholder:text-[#9a9a9a] ${className}`}
+      />
+    </div>
+  );
+}
