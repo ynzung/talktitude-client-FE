b70ca504ca9269110630a92d68d16526e4cd2ea0
ynzung <syj91105@sookmyung.ac.kr>
2025-06-04 18:50:32 +0900

    feat: Header 컴포넌트 뒤로 가기 연결

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import WhiteLogo from '/public/logo/white-logo.svg';
 import Image from 'next/image';
 import { IoArrowBackOutline } from 'react-icons/io5';
+import { useRouter } from 'next/navigation';
 
 interface ChatHeaderPropTypes {
   children?: React.ReactNode;
@@ -12,6 +13,11 @@ export default function Header({
   children,
   isChat = false,
 }: ChatHeaderPropTypes) {
+  const router = useRouter();
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
     <div className="w-full max-w-[600px] fixed z-10">
       <div className=" h-14 bg-mainColor flex items-center justify-start px-[22px]">
@@ -26,7 +32,11 @@ export default function Header({
       {isChat && (
         <div className="w-full max-w-[600px] h-14 bg-bgLightBlue flex items-center justify-start px-[22px] border border-t-0 border-lineGrey">
           <button className="flex items-center justify-center w-7 h-7">
-            <IoArrowBackOutline size={30} color="#3b3b3b" />
+            <IoArrowBackOutline
+              size={30}
+              color="#3b3b3b"
+              onClick={handleBack}
+            />
           </button>
           <div className="absolute left-1/2 -translate-x-1/2 text-textBlack text-xl font-bold">
             {children}
