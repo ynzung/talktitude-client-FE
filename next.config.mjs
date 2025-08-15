5f97416fe4cce2bfacff492ab2bcfc2d2ffe12c0
ynzung <syj91105@sookmyung.ac.kr>
2025-08-15 23:09:01 +0900

    chore: 이미지 도메인 허용 목록에 i.pinimg.com 추가 (#27)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
