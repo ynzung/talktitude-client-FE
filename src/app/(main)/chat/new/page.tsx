51448006c47d34f2853d65270b2cb2a70720a7ca
ynzung <syj91105@sookmyung.ac.kr>
2025-08-15 23:47:47 +0900

    refactor: ChatNewPage에서 mockOrders 제거 및 API 연동 (#27)

--- src/app/(main)/chat/new/page.tsx
+++ src/app/(main)/chat/new/page.tsx
@@ -1,19 +1,29 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '@/components/common/Header';
 import Layout from '@/components/common/Layout';
 import ChatOrderList from '@/components/chat/chatNew/ChatOrderList';
-import Link from 'next/link';
-import { mockOrders } from '@/lib/api/mock/orders';
+import { ChatOrderListItemPropsType } from '@/types/chat';
+import { getChatOrderList } from '@/api/chatApi';
 
 export default function ChatNewPage() {
+  const [orderList, setOrderList] = useState<ChatOrderListItemPropsType[]>([]);
+
+  useEffect(() => {
+    const fetchOrderList = async () => {
+      const response = await getChatOrderList();
+      setOrderList(response.data);
+    };
+    fetchOrderList();
+  }, []);
+
   return (
     <div className="bg-bgLightBlue">
       <Header isChat={false} />
       <Layout padding="pt-14 pb-20 border-x border-lineGray">
         <div className="mt-5">
-          <ChatOrderList items={mockOrders} />
+          <ChatOrderList items={orderList} />
         </div>
         <div className="mt-20 text-center underline text-textLightGray text-sm font-bold">
           <Link href="/chat/:id">주문 말고 다른 문의가 있어요</Link>
