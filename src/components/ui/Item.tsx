b5b54f41b0881f7d18e56063e44ac18e9a564c54
ynzung <syj91105@sookmyung.ac.kr>
2025-08-15 23:43:53 +0900

    refactor: Item 컴포넌트에 mode prop 추가하여 chatItem, orderItem 구분 (#27)

--- src/components/ui/Item.tsx
+++ src/components/ui/Item.tsx
@@ -2,48 +2,46 @@
 
 import React from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { ChatItemPropsType, ChatOrderItemPropsType } from '@/types/chat';
 
-interface ItemPropTypes {
-  data: {
-    sessionId: number;
-    status: string;
-    storeName: string;
-    storeImageUrl: string;
-    lastMessage: string;
-    orderSummary: string;
-  };
-}
+type ChatItemProps = ChatItemPropsType | ChatOrderItemPropsType;
 
-export default function Item({ data }: ItemPropTypes) {
-  const router = useRouter();
-  const { sessionId, storeName, storeImageUrl, lastMessage, orderSummary } =
-    data;
-
-  const handleClick = () => {
-    router.push(`/chat/${sessionId}`);
-  };
+export default function Item(props: ChatItemProps) {
+  const { imageSrc, title, summary, subText } =
+    props.mode === 'chat'
+      ? {
+          imageSrc: props.chatListData.storeImageUrl,
+          title: props.chatListData.storeName,
+          summary: props.chatListData.orderSummary,
+          subText: props.chatListData.lastMessage,
+        }
+      : {
+          imageSrc: props.orderData.restaurantImageUrl,
+          title: props.orderData.restaurantName,
+          summary: props.orderData.orderSummary,
+          subText: props.orderData.orderDate,
+        };
 
   return (
     <div
-      onClick={handleClick}
+      onClick={props.onClickChatItem}
       className="w-full px-4 py-5 bg-White border-b border-lineGray flex flex-row gap-3.5 overflow-hidden cursor-pointer last:border-b-0 hover:bg-[#f5f5f5]"
     >
       <div className="w-20 h-20 relative flex-shrink-0">
         <Image
-          src={storeImageUrl}
-          alt="가게 사진"
-          fill
+          src={imageSrc}
+          alt={`${title} 이미지`}
+          width={80}
+          height={80}
+          priority
           className="object-cover rounded-md"
         />
       </div>
       <div className="flex flex-col gap-1 justify-center">
-        <div className="text-TextBlack text-lg font-bold ">{storeName}</div>
-        <div className=" text-TextBlack text-sm font-normal">
-          {orderSummary}
-        </div>
+        <div className="text-TextBlack text-lg font-bold">{title}</div>
+        <div className="text-TextBlack text-sm font-normal">{summary}</div>
         <div className="text-textLightGray text-sm font-normal line-clamp-1">
-          {lastMessage}
+          {subText}
         </div>
       </div>
     </div>
