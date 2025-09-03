a2a366d353195d59bf9ac456679d4a4748d00937
ynzung <syj91105@sookmyung.ac.kr>
2025-09-03 22:59:47 +0900

    fix: item 컴포넌트 내의 이미지에서 발생하는 오류 해결 (#35)

--- src/components/ui/Item.tsx
+++ src/components/ui/Item.tsx
@@ -27,24 +27,27 @@ export default function Item(props: ChatItemProps) {
       onClick={props.onClickChatItem}
       className="w-full px-4 py-5 bg-White border-b border-lineGray flex flex-row gap-3.5 overflow-hidden cursor-pointer last:border-b-0 hover:bg-[#f5f5f5]"
     >
-      <div className="w-20 h-20 relative flex-shrink-0">
+      <div className="relative flex-shrink-0 w-20 h-20">
         <Image
           src={
             imageSrc ||
             'https://i.pinimg.com/736x/01/ae/fb/01aefbecec9bf875694c2f5b4dee0117.jpg'
           }
           alt={`${title} 이미지`}
-          width={80}
-          height={80}
+          fill
+          sizes="80px"
+          unoptimized={true}
           priority
           className="object-cover rounded-md"
         />
       </div>
       <div className="flex flex-col gap-1 justify-center">
         <div className="text-TextBlack text-lg font-bold">
-          {title || '기타 문의'}
+          {title || '주문 외 문의'}
         </div>
-        <div className="text-TextBlack text-sm font-normal">{summary}</div>
+        <div className="text-TextBlack text-sm font-normal">
+          {summary === '주문 외 문의' ? '기타 문의' : summary}
+        </div>
         <div className="text-textLightGray text-sm font-normal line-clamp-1">
           {subText}
         </div>
